f20fca42ac99394c6eca92b517967924befe1ebc
lincoln gibson <lincolngibson7@gmail.com>
2023-05-01 13:57:51 -0300

    Import luxon as module, create funnction to display formatted time.

---
+++ modules/luxonTime.js
@@ -0,0 +1,16 @@
+import { DateTime } from '../node_modules/luxon/src/luxon.js';
+
+const main = document.querySelector('main');
+const timeSlot = document.createElement('p');
+const section = document.querySelector('section');
+timeSlot.classList.add('localTime');
+
+export const showTime = () => {
+	setInterval(() => {
+		const now = DateTime.now();
+		const formattedDateTime = now.toFormat('MMMM dd, yyyy - HH:mm:ss a');
+		timeSlot.textContent = `Local Time: ${formattedDateTime}`;
+	}, 1000);
+
+	main.insertBefore(timeSlot, section);
+};
