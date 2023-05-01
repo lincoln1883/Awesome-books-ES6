17ddb83734fffe6c9a1547fbee6198ec8e31dfea
lincoln gibson <lincolngibson7@gmail.com>
2023-05-01 13:23:28 -0300

    Add functionality to removeBook module with ES6 import/export.

---
+++ modules/removeBook.js
@@ -0,0 +1,16 @@
+import { books, addBookToList } from './addBook.js';
+
+const removeBookFromList = (id) => {
+	books.splice(id, 1);
+	localStorage.setItem('books', JSON.stringify(books));
+	addBookToList();
+};
+
+const handleRemoveButtonClick = (e) => {
+	if (e.target.matches('.remove')) {
+		const id = e.target.dataset;
+		removeBookFromList(id);
+	}
+};
+
+export { handleRemoveButtonClick, removeBookFromList };
