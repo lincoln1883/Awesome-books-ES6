f5b921a5d90bb18dfe57abbcc76ccc2894da6af6
lincoln gibson <lincolngibson7@gmail.com>
2023-05-01 13:59:10 -0300

    Add import statment from time module into index file.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { handleFormSubmit, addBookToList } from './modules/addBook.js';
 import { handleRemoveButtonClick } from './modules/removeBook.js';
 import { handleNavigation, toggleMenu } from './modules/navigation.js';
 import Book from './modules/book.js';
+import { showTime } from './modules/luxonTime.js';
 
 const formButton = document.querySelector('#submit-button');
 const bookList = document.querySelector('.book-display');
@@ -19,3 +20,10 @@ handleNavigation(navList, sections);
 const hamburger = document.querySelector('.hamburger');
 const navLists = document.querySelector('.nav-lists');
 toggleMenu(hamburger, navLists);
+
+const main = document.querySelector('main');
+const timeSlot = document.createElement('p');
+const section = document.querySelector('section');
+timeSlot.classList.add('localTime');
+
+showTime(main, timeSlot, section);
