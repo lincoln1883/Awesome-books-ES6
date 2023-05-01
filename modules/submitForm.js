5a92c953f9c7b8024f6eea6f547ccda0ef2e2075
lincoln gibson <lincolngibson7@gmail.com>
2023-05-01 13:25:11 -0300

    Add functionality to submitForm module with ES6 import/export.

---
+++ modules/submitForm.js
@@ -0,0 +1,9 @@
+import { handleFormSubmit } from './addBook.js';
+
+const formButton = document.querySelector('#submit-button');
+
+const submitForm = () => {
+	formButton.addEventListener('click', handleFormSubmit);
+};
+
+export default submitForm;
